b733ffb294fd5403755a8265dde11e44ecfa4473
erik <erikwasmosy98@gmail.com>
2024-06-11 14:17:53 -0400

    Initial Commit

---
+++ close Watch App/closeApp.swift
@@ -0,0 +1,17 @@
+//
+//  closeApp.swift
+//  close Watch App
+//
+//  Created by Erik Wasmosy on 2024-06-11.
+//
+
+import SwiftUI
+
+@main
+struct close_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
